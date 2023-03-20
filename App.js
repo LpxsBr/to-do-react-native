146f76e7669def0e736bdc4248e2d4972f5238eb
LpxsBr <anselmolopes.an@gmail.com>
2023-03-20 11:21:10 -0300

    add or update styles and components

--- App.js
+++ App.js
@@ -1,63 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Switch, Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [activitie, setActivitie] = useState([])
   const [toDo, setToDo] = useState([])
+  const [empty, setEmpty] = useState(null)
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+
+  const activitieHandler = (event) => {
+    setActivitie(String(event))
+    setEmpty()
+  };
+  const toDoHandler = () => {
+    if (!activitie) {
+      setEmpty('Campo vázio :)')
+    }
+    else {
+      setToDo((prev) => [...prev, activitie])
+      setActivitie()
+    }
+  }
+
+  const cleanInputHandler = () => setToDo([]);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View
-      style={{width: 300,display: 'flex', gap: 10}}>
-        <TextInput
         style={{
           width: 300,
-          height: 50,
-          backgroundColor: '#f1f1f1',
-          padding: 10,
-          borderWidth: 1
-        }}
-        onChangeText={(event)=>{
-          console.log(event)
-          setActivitie(event)
-        }} value={activitie}></TextInput>
+          display: 'flex',
+          gap: 10
+        }}>
+        <TextInput
+          style={{
+            width: 300,
+            height: 50,
+            backgroundColor: '#f1f1f1',
+            padding: 10,
+            borderWidth: 1
+          }}
+          onChangeText={activitieHandler}
+          value={activitie}></TextInput>
+
+        <Text
+          style={{
+            color: 'red'
+          }}>{empty}</Text>
 
         <Button
-        style={{width: 300, height: 50}}
-        title='add'
-        onPress={()=>{
-          setToDo((prev)=>[...prev, activitie])
-          setActivitie()
-          }}></Button>
+          style={{ width: 300, height: 50 }}
+          title='add'
+          onPress={toDoHandler}></Button>
 
         <Button
-        style={{width: 300, height: 50}}
-        title='remove all'
-        onPress={()=>{
-          setToDo([])
-          }}></Button>
-          
-        <View>
+          style={{ width: 300, height: 50 }}
+          title='remove all'
+          onPress={cleanInputHandler}></Button>
+
+        <View style={{
+          display: 'flex',
+          gap: 10
+        }}>
           {
-            toDo.map((activie)=>(
-              <Text
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                textAlign: 'center',
-                textAlignVertical: 'center',
-                borderRadius: 10,
-                width: 300,
-                height: 40,
-                backgroundColor: 'pink',
-                color: 'white'
-              }}>{activie}</Text>))
+            !toDo
+              ? <Text>Nenhum item aqui</Text>
+              : toDo.map((activie, key) => (
+                <Pressable
+                  key={key}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    borderRadius: 10,
+                    width: 300,
+                    height: 40,
+                    backgroundColor: `pink`,
+                    color: 'white'
+                  }}>
+                  <Text
+                    style={{
+                      textAlignVertical: 'center',
+                      width: 200,
+                      padding: 10
+                    }}>{activie}</Text>
+                  <Switch
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                  />
+                </Pressable>))
           }
         </View>
       </View>
-      
+
     </View>
   );
 }
